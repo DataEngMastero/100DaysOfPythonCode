eec7491b86475ebea1b77630a4c3346c43721b09
Pooja Singh <poojasingh@Poojas-MacBook-Air.local>
2023-11-20 22:01:01 +0530

    Completed Blackjack game

--- Beginners/11_Blackjack.py
+++ Beginners/11_Blackjack.py
@@ -13,62 +13,62 @@ logo = """
               `------'
 """
 
-cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-def calculate_score(cards_list):
-    score = 0
-    for card in cards_list:
-        if card in cards[:-4]: 
-            score += int(card)
-        elif card in cards[-4:-1]:
-            score += 10
-        elif score < 11 and card == 'A':
-            score += 11
-        else:
-            score += 1
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
     
-    return score
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
 
-# Ask user if they want to play - TODO
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n' : ") == 'y':
-    # Print logo
-    clear()
-    print(logo)
-        
-    # Random Selection of 2 Cards and Calculate Score
+    return sum(cards)
+
+def calculate_winner(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, Opponent has blackjack"
+    elif user_score == 0:
+        return "Win with a blackjack"
+    elif user_score > 21:
+        return "You went over. You lose."
+    elif computer_score > 21:
+        return "Opponent went over. You win."
+    elif user_score > computer_score:
+        return "You win."
+    else:
+        return "You lose."
+
+def play_game():
+    is_game_ended = False
     user_cards = random.sample(cards, 2)
     computer_cards =  random.sample(cards, 2)
-
-    draw_card = True
-    while draw_card:
+    while not is_game_ended:
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
 
-        # Show User Cards and its core and Computer's first card
         print(f"Your cards: {user_cards}, Current Score: {user_score}")
         print(f"Computer's first card: {computer_cards[0]}")
 
-        # Ask User for Another-Pass
-        another_card = input("Type 'y' to get another card, type 'n' to pass : ").lower()
-        if another_card == 'n' or user_score > 21 or computer_score > 21:
-            draw_card = False
-        elif another_card == 'y':
-            user_cards.append(random.choice(cards))
-            computer_cards.append(random.choice(cards))
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_ended = True
+        else:
+            another_card = input("Type 'y' to get another card, type 'n' to pass : ").lower()
+            if another_card == 'y':
+                user_cards.append(random.choice(cards))
+            else:
+                is_game_ended = True
 
-            
-    # Evaluate final score of User and Computer
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-    print(f"Your final hand: {user_cards}, Final Score: {user_score}")
-    print(f"Computer's final hand: {computer_cards}, Final Score : {computer_score}")
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(random.choice(cards))
+        computer_score = calculate_score(computer_cards)      
 
-    # Declare Winner - TODO
-    if user_score > 21:
-        print("You went over. You lose!")
-    elif computer_score > 21:
-        print("Opponent went over. You winss!")
-    elif user_score > computer_score:
-        print('You wins!')
-    else:
-        print('Computer wins!')
+    print(f"    Your final hand is {user_cards}, final_score = {user_score}")
+    print(f"    Computer's final hand is {computer_cards}, final_score = {computer_score}")
+    print(calculate_winner(user_score,computer_score))
+
+while input("Do you want to play a game of blackjack ? Type 'y' for yes, 'n' for no  ? ") == "y":
+    play_game()
